39ae1a3160b575f1e8909ad503163e7c57aadfc5
Remo mbp <remo.val@blue.ch>
2022-08-29 23:31:52 +0200

    some more styled components

--- mimir/src/Navbar.tsx
+++ mimir/src/Navbar.tsx
@@ -2,16 +2,41 @@ import styled from 'styled-components/macro'
 
 export const Navbar = () => {
     return (
-        <MyNav>hallo</MyNav>
-        
+        <MyNav fancy>
+            <NavTitle>mimir</NavTitle>
+            <StatusPill>Status</StatusPill>
+            <NavTitle>Manage</NavTitle>
+        </MyNav>
     );
 }
 
-const MyNav = styled.nav`
+interface NavbarProps { fancy?: Boolean }
+
+const MyNav = styled.nav<NavbarProps>`
     top: 0;
     width: 100%;
+    background-color: ${p => p.fancy ? 'purple' : 'darkgreen'};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const NavTitle = styled.div`
     padding: 20px;
-    background-color: blue;
     font-size: calc(10px + 2vmin);
     color: white;
+`
+
+const StatusPill = styled.div`
+    background-color: #4CAF50;
+    border: none;
+    border-radius: 12px;
+    color: white;
+    padding: 12px 28px;
+    text-align: center;
+    text-decoration: none;
+`
+
+const ManageLink = styled.div`
+    
 `
