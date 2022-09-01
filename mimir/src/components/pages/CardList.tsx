9dbf6b00d12cd45b581fc42de6d9f321e891c4e1
Remo mbp <remo.val@blue.ch>
2022-09-01 23:59:50 +0200

    added fetch from backend

--- mimir/src/components/pages/CardList.tsx
+++ mimir/src/components/pages/CardList.tsx
@@ -1,17 +1,55 @@
 import { CardData } from 'data/CardData';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro'
 
 export const CardList = () => {
+
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const actualData = await fetch(
+        `/api/cards/`
+      ).then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `Backend HTTP error: Status ${response.status}`
+          );
+        }
+        return response.json();
+      }).then((actualData) => {
+        setData(actualData);
+        setError(null);
+      }).catch((err) => {
+        setError(err.message);
+        setData([]);
+      });
+    }
+    getData()
+  }, [])
+
   return (
     <Main>Here could be our CardList
+      {error && (
+        <div>{error}</div>
+      )}
       <OverviewList>
-        {CardData.map(data => (
-          <OverviewListItem>{data.id} ({data.front} / {data.back})<Link to={"/cards/" + data.id}>Edit</Link></OverviewListItem>
+        {data && data.map((dt: cardItem) => (
+          <OverviewListItem key={dt.id}>
+            {dt.id} ({dt.front} / {dt.back})<Link to={"/cards/" + dt.id}>Edit</Link>
+          </OverviewListItem>
         ))}
       </OverviewList>
     </Main>
   );
+}
+
+type cardItem = {
+  id: string,
+  front: string,
+  back: string
 }
 
 const Main = styled.main`
