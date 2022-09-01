d278014d9fa1318d1eae1f7895bf285eedc4d193
Remo mbp <remo.val@blue.ch>
2022-09-01 19:11:44 +0200

    added browser-router

--- mimir/src/App.tsx
+++ mimir/src/App.tsx
@@ -1,13 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
 import React from "react";
 import logo from "./logo.svg";
 import { Navbar } from "./Navbar"
+import { Home } from "./Home"
 import "./App.css";
+import { Layout } from 'Layout';
 
 
 export default function App() {
   return (
     <>
-    <Navbar />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </>
   )
 }
