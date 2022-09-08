157a1a2f7237b1f56f591d6b6532ae4057924978
Damian Frizzi <damian.frizzi@truewealth.ch>
2022-09-08 17:43:55 +0200

    Simplify fetch

--- mimir/src/components/pages/CardList.tsx
+++ mimir/src/components/pages/CardList.tsx
@@ -1,68 +1,63 @@
-import { CardData } from 'data/CardData';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components/macro'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components/macro";
 
 export const CardList = () => {
-
   const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(`/api/cards/`);
+      const json = await res.json();
+
+      if (!res.ok) {
+        throw new Error(`Backend HTTP error: Status ${res.status}`);
+      }
+
+      setData(json);
+    } catch (err) {
+      setError("Something went wrong");
+    }
+  };
 
   useEffect(() => {
-    async function getData() {
-      const actualData = await fetch(
-        `/api/cards/`
-      ).then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            `Backend HTTP error: Status ${response.status}`
-          );
-        }
-        return response.json();
-      }).then((actualData) => {
-        setData(actualData);
-        setError(null);
-      }).catch((err) => {
-        setError(err.message);
-        setData([]);
-      });
-    }
-    getData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
-    <Main>Here could be our CardList
-      {error && (
-        <div>{error}</div>
-      )}
+    <Main>
+      Here could be our CardList
+      {error && <div>{error}</div>}
       <OverviewList>
-        {data && data.map((dt: cardItem) => (
-          <OverviewListItem key={dt.id}>
-            {dt.id} ({dt.front} / {dt.back})<Link to={"/cards/" + dt.id}>Edit</Link>
-          </OverviewListItem>
-        ))}
+        {data &&
+          data.map((dt: cardItem) => (
+            <OverviewListItem key={dt.id}>
+              {dt.id} ({dt.front} / {dt.back})
+              <Link to={"/cards/" + dt.id}>Edit</Link>
+            </OverviewListItem>
+          ))}
       </OverviewList>
     </Main>
   );
-}
+};
 
 type cardItem = {
-  id: string,
-  front: string,
-  back: string
-}
+  id: string;
+  front: string;
+  back: string;
+};
 
 const Main = styled.main`
   margin: 10px;
-`
+`;
 
 const OverviewList = styled.ul`
   list-style: none;
   margin: 10px;
   padding: 0px;
-`
+`;
 
 const OverviewListItem = styled.li`
   margin: 3px;
-  
-`
+`;
