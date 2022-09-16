a345fb7e6a485d38d73116102cc27e5c7c9820ee
Remo mbp <remo.val@blue.ch>
2022-09-16 09:17:39 +0200

    finished game logic

--- mimir/src/components/pages/Home.tsx
+++ mimir/src/components/pages/Home.tsx
@@ -1,9 +1,11 @@
-import { fetchGame, startGame } from 'api/mimir-backend';
+import { fetchGame, setAnswer, startGame, clearGame} from 'api/mimir-backend';
 import { AppContext } from 'data/Context';
-import { useContext, useEffect } from 'react';
+import { Game } from 'models/Game';
+import { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components/macro'
 
 export const Home = () => {
+  const [input, setInput] = useState('')
   const { game, dispatch } = useContext(AppContext)
 
   useEffect(() => {
@@ -19,15 +21,62 @@ export const Home = () => {
     dispatch({ type: 'update-game', game })
   }
 
+  const answer = async () => {
+    const game = await setAnswer(input)
+    dispatch({ type: 'update-game', game })
+    setInput('')
+  }
+
+  const clear = async () => {
+    const game = await clearGame()
+    dispatch({ type: 'update-game', game })
+  }
+
+  const reset = async () => {
+    await clearGame()
+    const game = await startGame()
+    dispatch({ type: 'update-game', game })
+  }
+
+  if (!game) {
+    return (
+      <Main>
+        No Game running!
+        <button onClick={() => start()}>Start Game</button>
+      </Main>
+    )
+  }
+
+  if (game.cardCount === game.solved.length) {
+    return (
+      <Main>
+        Game finished!
+        <p>Solved correct: {game.solved.filter(x => x.accepted === true).length} / {game.cardCount}</p>
+        {game.solved.map(answer =>
+          <p>{answer.front} / {answer.back} / {answer.answer} / {answer.accepted ? 'correct' : 'wrong'}</p>
+        )}
+        <div><button onClick={() => reset()}>Start new Game</button></div>
+      </Main>
+
+    )
+  }
+
+  const percent = Math.floor(100 /  game.cardCount * game.solved.length)
   return (
     <Main>
-      You're home!
-      <p>Progress: {game?.solved.length} / {game?.cardCount}</p>
-      {game !== undefined ? game.front : <button onClick={() => start()}>Start Game</button>}
+      Game running!
+      <p>Progress: {percent} %</p>
+      <p>front: {game.front}</p>
+      <div><Input onChange={e => setInput(e.target.value)} value={input} /></div>
+      <div><button onClick={() => answer()}>Submit</button> | <button onClick={() => clear()}>Delete Game</button></div>
     </Main>
   )
 }
 
 const Main = styled.main`
   margin: 10px;
+`
+
+const Input = styled.input`
+  
 `
