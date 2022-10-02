67526123939f9837bf32eaf523f40fe17b0d86a2
Remo mbp <remo.val@blue.ch>
2022-10-02 23:22:19 +0200

    remove index.css

--- mimir/src/components/GlobalStyles.ts
+++ mimir/src/components/GlobalStyles.ts
@@ -6,6 +6,19 @@ export const GlobalStyle = createGlobalStyle`
   font-size: 16px;
 }
 
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+
 *, *:before, *:after {
   box-sizing: inherit;
 }
--- mimir/src/index.tsx
+++ mimir/src/index.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { AppContext, AppProvider } from "data/Context";
+import { AppProvider } from "data/Context";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
